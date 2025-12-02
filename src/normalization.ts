c49372a911c58c173318d79990ed378d175796ea
hopperelec <hopperelec@gmail.com>
2025-12-02 23:58:10 +0000

    Normalize X and ? to x

--- src/normalization.ts
+++ src/normalization.ts
@@ -11,17 +11,17 @@ export function normalizeTRN(trn: TRN): TRN {
 
 // --- Description ---
 
-const CLASS_555_FORMATTING_REGEX = new RegExp(/(?<!\d)(555|5) ?((\d|\?|x){3})(?!\d)/g);
-const METROCAR_FORMATTING_REGEX = new RegExp(/(?<!\d)(599|994|4) ?[0x?]((\d|\?|x){2})(?!\d)/g);
+const CLASS_555_FORMATTING_REGEX = new RegExp(/(?<!\d)(555|5) ?((\d|\?|x){3})(?!\d)/gi);
+const METROCAR_FORMATTING_REGEX = new RegExp(/(?<!\d)(599|994|4) ?[0x?]((\d|\?|x){2})(?!\d)/gi);
 
 const NORMALIZED_METROCAR = "40(\\d|\\?|x){2}";
-const METROCAR_COUPLING_REGEX1 = new RegExp(`${NORMALIZED_METROCAR}( ${NORMALIZED_METROCAR})+`, "g");
+const METROCAR_COUPLING_REGEX1 = new RegExp(`${NORMALIZED_METROCAR}( ${NORMALIZED_METROCAR})+`, "gi");
 const METROCAR_COUPLING_REGEX2 = new RegExp(
     "(?<!\\+|\\d)" + // Not preceded by a + or digit
     NORMALIZED_METROCAR + // At least one metrocar
     `( \\+ ${NORMALIZED_METROCAR})+` + // Followed by one or more metrocars separated by " + "
     "(?!\\+|\\d)", // Not followed by a + or digit
-    "g"
+    "gi"
 );
 const METROCAR_COUPLING_REGEX3 = new RegExp(
     "(?<=" + // Don't include first unit in match
@@ -33,7 +33,7 @@ const METROCAR_COUPLING_REGEX3 = new RegExp(
     NORMALIZED_METROCAR + // Followed by a metrocar
     "(?!\\+|\\d)" + // Not followed by a + or digit
     ")",
-    "g"
+    "gi"
 );
 
 const DISCORD_EMOJI_FORMAT = "<a?:\\w+:\\d+> ?";
@@ -41,19 +41,23 @@ const CLASS_555_EMOJI_REGEX = new RegExp(
     `(?<!\\d|${DISCORD_EMOJI_FORMAT})` + // Not preceded by a digit or an existing emoji
     "555\\d(\\d|\\?|x){2}" + // This is the only part included in the match
     "(?!\\d|x)", // Not followed by a digit or x
-    "g"
+    "gi"
 );
 const METROCAR_EMOJI_REGEX = new RegExp(
     `(?<!\\+|\\d|${DISCORD_EMOJI_FORMAT})` + // Not preceded by a +, digit, or an existing emoji
     NORMALIZED_METROCAR + // At least one metrocar
     `(\\+${NORMALIZED_METROCAR})*` + // Followed by one or more metrocars separated by "+"
     "(?!\\+|\\d)", // Not followed by a + or digit
-    "g"
+    "gi"
 );
+
+const UNKNOWN_UNIT_REGEX = new RegExp(/([x?])/gi);
 
 function addEmojis(unitEmoji: string) {
     // Prepends unitEmoji and, if the unit contains 'x', a question mark emoji
-    return (unit: string) => unit.includes('x') ? `:question:<${unitEmoji}> ${unit}` : `<${unitEmoji}> ${unit}`;
+    return (unit: string) => UNKNOWN_UNIT_REGEX.test(unit)
+        ? `:question:<${unitEmoji}> ${unit.replaceAll(UNKNOWN_UNIT_REGEX, 'x')}`
+        : `<${unitEmoji}> ${unit}`;
 }
 
 export function normalizeDescription(description: string) {
